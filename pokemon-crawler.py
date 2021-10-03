560bdde3c994088a5d4e257a6421d781aecb2637
Victor Mari <vrodriguez@tmforum.org>
2021-10-03 14:27:27 +0100

    grouped api indexes

--- pokemon-crawler.py
+++ pokemon-crawler.py
@@ -1,37 +1,44 @@
+from os import name
 import requests
 import sys
 import json
 from pathlib import Path
 
-def get_pokemon(pokemon_id):
-    req_data = {
-        "url": f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}",
-    }
 
-    try:
-        req = requests.get(**req_data)
-        req.raise_for_status()
-        if req.status_code == 200:
-            return req.json()
-    except Exception as e:
-        print(e)
-        print("Cound not get pokemon ", pokemon_id)
+def crawl_index(index):
+    for resource in index["results"]:
+        req_data = {
+            "url": resource['url'],
+        }
+
+        try:
+            req = requests.get(**req_data)
+            req.raise_for_status()
+            if req.status_code == 200:
+                return [req.json()]
+        except Exception as e:
+            print(e)
+            print("Cound not get resource", resource["name"], "at url:", resource["url"])
 
 
-def save_pokemon(pokemon_info):
-    file_name = f"{pokemon_info['id']}_{pokemon_info['name']}.json"
-    with Path(f"examples/pokemons/{file_name}").open("w") as f:
-        json.dump(pokemon_info, f, indent=4)
+def save_resource(resource, collection_name):
+    file_name = f"resource_{resource['id']}.json"
+    with Path(f"collections/{collection_name}/{file_name}").open("w") as f:
+        json.dump(resource, f, indent=4)
 
-def get_pokemon_index(start=0, end=0):
-    with Path("examples/pokemon-list.json").open("r") as f:
-        return json.load(f)
+
+def get_indexes(start=0, end=0):
+    for index in Path("indexes").glob("*.json"):
+        with index.open("r") as f:
+            yield (json.load(f), index.name[:-5])
+
 
 def main():
-    for pokemon in get_pokemon_index()["results"]:
-        pok_info = get_pokemon(pokemon['url'].split("/")[-2])
-        save_pokemon(pok_info)
-    return 0
+    for index, collection_name in get_indexes():
+        print("processing collection", collection_name)
+        for resource in crawl_index(index):
+            print("element id", resource["id"])
+            save_resource(resource, collection_name)
 
 
 if __name__ == "__main__":
